6f99cce580bd624f4f5aa1382d5dc07dc628ad3c
Sarwar Khalid <56037779+Nekrodeath@users.noreply.github.com>
2021-09-10 21:58:32 +0600

    Delete sarwar

--- frontend-php/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-function redirect($url)
-		{
-			echo("<script>location.href = '$url'</script>");
-		}
--- frontend-php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION["handle"] = "";
-$_SESSION["id"] = -1;
-require_once("./functions.php");
-redirect("index.php");
